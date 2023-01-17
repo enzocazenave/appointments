2ee3664f08e4160cc8264ad4eff735846da818c3
Enzo Cazenave <encaze24@hotmail.com>
2023-01-17 14:25:21 -0300

    Add image in home page that shows some shops

--- turnos-web/src/appointments/pages/HomePage.jsx
+++ turnos-web/src/appointments/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from '../../styles/appointments/pages/HomePage.module.css';
 import { Calendar } from '../../svgs/Calendar';
 import { Shop } from '../../svgs/Shop';
@@ -11,6 +12,31 @@ export const HomePage = () => {
                 <DataItem title="Comercios adheridos" icon={ <Shop width={ 60 } height={ 60 } /> }  data={ 6 } />
                 <DataItem title="Turnos reservados" icon={ <Calendar width={ 50 } height={ 50 } /> } data={ 1239 }  />
                 <DataItem title="Usuarios creados" icon={ <UserIcon width={ 50 } height={ 50 } /> }  data={ 357 } />
+            </div>
+
+            <div>
+                <h1 className={ styles.title }>Algunos comercios adheridos</h1>
+
+                <div className={ styles.shops }>
+                    <Link
+                        to="/shop/1"
+                        className={ styles.shop }
+                        style={{
+                            background: 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(https://b2743180.smushcdn.com/2743180/wp-content/uploads/sites/10/2022/03/descarga-2.jpeg?lossy=1&strip=1&webp=1)'
+                        }}
+                    >
+                        <h2>Sizo Gerard</h2>
+                    </Link>
+                    <Link 
+                        to="/shop/2"
+                        className={ styles.shop }
+                        style={{
+                            background: 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(https://assets.iprofesional.com/assets/jpg/2022/06/538177.jpg)'
+                        }}
+                    >
+                        <h2>Lavadero Mitre</h2>
+                    </Link>
+                </div>
             </div>
         </div>
     )
