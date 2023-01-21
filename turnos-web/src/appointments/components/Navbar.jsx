e1c7a85b7612bebd5738888bef09343e86a7079d
Enzo Cazenave <encaze24@hotmail.com>
2023-01-21 02:29:52 -0300

    Change in navbar.jsx

--- turnos-web/src/appointments/components/Navbar.jsx
+++ turnos-web/src/appointments/components/Navbar.jsx
@@ -2,11 +2,26 @@ import styles from '../../styles/appointments/components/Navbar.module.css';
 import { Calendar } from '../../svgs/Calendar';
 import { Profile } from '../../svgs/Profile';
 import { MagnifyingGlass } from '../../svgs/MagnifyingGlass';
-import '../../helpers/navbar';
+import { useEffect, useState } from 'react';
 
 export const Navbar = () => {
+
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setActive(scrollY > (innerHeight / 2));
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
+
     return (
-        <nav className={ styles.container } id="navmain">
+        <nav className={ `${styles.container} ${ active ? 'navbar_active' : '' }` } id="navmain">
             <Calendar width={ 30 } height={ 30 } fill={ '#00CC8F' } />
 
             <div className={ styles.right }> 
--- turnos-web/src/helpers/navbar.js
+++
@@ -1,5 +0,0 @@
-const navbar = document.querySelector('#navmain');
-
-window.addEventListener('scroll', () => {
-    navbar.classList.toggle('navbar_active', scrollY > (innerHeight / 2));
-});
