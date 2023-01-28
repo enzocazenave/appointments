08c43f35d04b86d45c350e152e6e40c8520df255
Enzo Cazenave <encaze24@hotmail.com>
2023-01-28 15:02:55 -0300

    Add changes

--- turnos-web/src/appointments/pages/ShopPage.jsx
+++ turnos-web/src/appointments/pages/ShopPage.jsx
@@ -28,9 +28,8 @@ export const ShopPage = () => {
     }, [shopId]);
 
     const [formValues, setFormValues] = useState({
-        title: '',
-        notes: '',
-        appointment: '',
+        comment: '',
+        appointment: ''
     });
 
     const onDateChanged = (event, changing) => {
@@ -132,28 +131,48 @@ export const ShopPage = () => {
                         onCancel={ () => setIsModalOpen(false) }
                         onClose={ () => setIsModalOpen(false) }
                     >
-                        <h1>Reservá tu turno</h1>
-                        <label>Fecha y hora del turno</label>
-                        <DatePicker 
-                            minDate={ new Date()  }
-                            minTime={setHours(setMinutes(new Date(), 0), 8)}
-                            maxTime={setHours(setMinutes(new Date(), 45), 19)}
-                            //*excludeTimes={[new Date("2023-01-31T11:15:00.000Z")]} ESTO CANCELA LA HORA 8.15 (GMT-3)
-                            //*excludeDates={ [new Date("2023-01-31T13:00:00.000Z")]} ESTO CANCELA EL DIA 31-01-2023
-                            timeIntervals={15}
-                            selected={ formValues.appointment } 
-                            onChange={ (event) => onDateChanged(event, 'appointment') }
-                            dateFormat="Pp"
-                            showTimeSelect        
-                            locale='es'
-                            timeCaption='Hora'
-                            fixedHeight
-                            filterDate={ isWeekend }
-                            withPortal
-                            shouldCloseOnSelect={false}
+                        <h1 className={ styles.createAppointmentModalTitle }>Reservá tu turno</h1>
+                        <p className={ styles.createAppointmentModalText }>
+                            Estas reservando un turno con Ariel, trabaja los martes y jueves de 8.00hs a 18.00hs
+                        </p>
+                        <div style={{ marginBottom: '3rem' }}>
+                            <label className={ styles.createAppointmentModalLabel }>Selecciona la fecha y hora del turno</label>
+                            <DatePicker 
+                                className={ styles.createAppointmentModalPicker }
+                                minDate={ new Date()  }
+                                minTime={setHours(setMinutes(new Date(), 0), 8)}
+                                maxTime={setHours(setMinutes(new Date(), 45), 19)}
+                                //*excludeTimes={[new Date("2023-01-31T11:15:00.000Z")]} ESTO CANCELA LA HORA 8.15 (GMT-3)
+                                //*excludeDates={ [new Date("2023-01-31T13:00:00.000Z")]} ESTO CANCELA EL DIA 31-01-2023
+                                timeIntervals={15}
+                                selected={ formValues.appointment } 
+                                onChange={ (event) => onDateChanged(event, 'appointment') }
+                                dateFormat="Pp"
+                                showTimeSelect        
+                                locale='es'
+                                timeCaption='Hora'
+                                fixedHeight
+                                filterDate={ isWeekend }
+                                placeholderText="Haz click aqui"
+                            />
+                        </div>
+                        <div style={{ display: 'flex', flexDirection: 'column', gap: '.5rem', marginBottom: '3rem' }}>
+                            <label className={ styles.createAppointmentModalLabel }>¿Quieres dejar un comentario?</label>
+                            <textarea 
+                                type="text" 
+                                className={ styles.createAppointmentModalInput }
+                                placeholder="Comentarios"
+                                value={ formValues.comment }
+                                onChange={ (e) => onDateChanged(e.target.value, 'comment') }
+                                name="comment"
+                            >
 
-                        />
-                        <button onClick={ () => setIsModalOpen(false) }>Salir</button>
+                            </textarea>
+                        </div>
+                        <div className={ styles.createAppointmentModalButtons }>
+                            <button className={ styles.createAppointmentModalConfirm } onClick={ () => setIsModalOpen(false) }>Reservar</button>
+                            <button className={ styles.createAppointmentModalClose } onClick={ () => setIsModalOpen(false) }>Salir</button>
+                        </div>
                     </dialog>
                 </div>
             )
