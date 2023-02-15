ab7b3395c15e568903d68a62bd6f5e11e35e90f5
Enzo <encaze24@hotmail.com>
2023-02-14 22:25:15 -0300

    Little change in backend

--- turnos-api/controllers/auth.js
+++ turnos-api/controllers/auth.js
@@ -179,6 +179,7 @@ const changeEmailConfirm = async(req, res = response) => {
         await user.save();
 
         const tokenPayload = {
+            _id: user._id,
             email: newEmail,
             name: user.name,
             surname: user.surname,
