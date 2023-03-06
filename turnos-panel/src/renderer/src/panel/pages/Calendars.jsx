45fc2536355a6722cc934d15c598a00e64ae226f
Enzo Cazenave <encaze24@hotmail.com>
2023-03-06 09:47:08 -0300

    Changes in calendars page at turnos-panel

--- turnos-panel/src/renderer/src/panel/pages/Calendars.jsx
+++ turnos-panel/src/renderer/src/panel/pages/Calendars.jsx
@@ -67,26 +67,96 @@ export const Calendars = () => {
                     </button>
                 </div>
                 <div className={ styles.menuCalendarsSelected }>
-                    <Avatar width={ 200 } height={ 200 } />
-                    <div className={ styles.inputContainer }>
-                        <label>Nombre</label>
-                        <input
-                            className={ styles.input }
-                            type="text"
-                            placeholder="Ariel"
-                        />
+                    <div className={ styles.menuFirstLine }>
+                        <Avatar width={ 200 } height={ 200 } />
+                        <div className={ styles.menuFirstLineRight }>
+                            <div className={ styles.inputContainer }>
+                                <label>Nombre</label>
+                                <input
+                                    className={ styles.input }
+                                    type="text"
+                                    placeholder="Ariel"
+                                />
+                            </div>
+                            <div className={ styles.inputContainer }>
+                                <label>Descripción</label>
+                                <textarea 
+                                    style={{ height: 100 }}
+                                    className={ styles.input }
+                                    placeholder="Estas reservando un turno con Ariel, trabaja los martes y jueves de 8.00hs a 18.00hs"
+                                ></textarea>
+                            </div>
+                        </div>
                     </div>
-                    <div className={ styles.inputContainer }>
-                        <label>Descripción</label>
-                        <textarea 
-                            style={{ height: 100 }}
-                            className={ styles.input }
-                            placeholder="Estas reservando un turno con Ariel, trabaja los martes y jueves de 8.00hs a 18.00hs"
-                        ></textarea>
-                    </div>
+
+                    <div className={ styles.menuSecondLine }>
+
+                        <div className={ styles.inputContainer }>
+                            <label>Días de atención</label>
+                            <div className={ styles.checkboxContainer}>
+                                <input
+                                    className={ styles.input }
+                                    type="checkbox"
+                                />
+                                <span>Lunes</span>
+                            </div>
+                            <div className={ styles.checkboxContainer}>
+                                <input
+                                    className={ styles.input }
+                                    type="checkbox"
+                                />
+                                <span>Martes</span>
+                            </div>
+                            <div className={ styles.checkboxContainer}>
+                                <input
+                                    className={ styles.input }
+                                    type="checkbox"
+                                />
+                                <span>Miercoles</span>
+                            </div>
+                            <div className={ styles.checkboxContainer}>
+                                <input
+                                    className={ styles.input }
+                                    type="checkbox"
+                                />
+                                <span>Jueves</span>
+                            </div>
+                            <div className={ styles.checkboxContainer}>
+                                <input
+                                    className={ styles.input }
+                                    type="checkbox"
+                                />
+                                <span>Viernes</span>
+                            </div>
+                            <div className={ styles.checkboxContainer}>
+                                <input
+                                    className={ styles.input }
+                                    type="checkbox"
+                                />
+                                <span>Sábado</span>
+                            </div>
+                            <div className={ styles.checkboxContainer}>
+                                <input
+                                    className={ styles.input }
+                                    type="checkbox"
+                                />
+                                <span>Domingo</span>
+                            </div>
+                        </div>
+
+                        <div className={ styles.menuFirstLineRight }>
+                            <div className={ styles.inputContainer }>
+                                <label>Frecuencia de turnos en minutos</label>
+                                <input
+                                    className={ styles.input }
+                                    type="text"
+                                    placeholder="30"
+                                />
+                            </div>
+                        </div>
+                    </div>   
                 </div>
             </div>
         </div>
-            
     )
 }   
